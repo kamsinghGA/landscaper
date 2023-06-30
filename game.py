dc98e393f85dd59363fa2cc3c78866610333680a
kamsinghGA <kamaljitga@gmail.com>
2023-06-29 22:55:22 -0400

    landscaper: user can use teeth to cut grass

---
+++ game.py
@@ -0,0 +1,10 @@
+tools = ['teeth']
+money = 0
+prices = {'scissors': 5, 'push lawnmower': 25, 'battery-powered lawnmower': 250, 'team of starving students': 500}
+game = True
+
+# Game loop
+while game == True:
+    print('Your current tools are: ' + tools)
+    print('The amount of money you have is: $' + str(money))
+    
